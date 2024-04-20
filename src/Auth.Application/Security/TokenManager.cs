05093a05c7d2e145b30a8e66c7d5c1664b3e86a9
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-20 23:00:00 +0500

    bugunchalik shu

--- src/Auth.Application/Security/TokenManager.cs
+++ src/Auth.Application/Security/TokenManager.cs
@@ -1,17 +1,31 @@
-﻿using Auth.Application.Security.Interfaces;
+﻿using System.Security.Cryptography;
+using Auth.Application.Security.Interfaces;
 using Auth.Core.Entities;
+using Microsoft.Extensions.Configuration;
 
 namespace Auth.Application.Security;
 
 public class TokenManager : ITokenManager 
 {
+    private readonly IConfiguration _configuration;
+
+    public TokenManager(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public async ValueTask<string> GenerateAccessToken(User user)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     public RefreshToken GeneratRefreshToken(User user)
     {
-        throw new NotImplementedException();
+        return new RefreshToken()
+        {
+            Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(256)),
+            Expires = DateTime.UtcNow.AddDays(
+                int.Parse(_configuration.GetSection("Authentication:RefreshTokenLifetimeDays").Value!)),
+            User = user
+        };
     }
 }
