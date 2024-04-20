b4551e9520354a58747e6229fafdd5842ebdf2be
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-20 19:26:34 +0500

    create common entity

---
+++ src/Auth.Core/Common/AuditableEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Auth.Core.Common;
+    public abstract class AuditableEntity<T> : BaseEntity<T>
+    {
+           public DateTime CreateAt { get; set; }
+           public DateTime UpdateAt { get; set; }
+    }
+
---
+++ src/Auth.Core/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Auth.Core.Common;
+
+    public abstract class BaseEntity<T>
+    {
+         public T Id { get; set; }
+    }
+
