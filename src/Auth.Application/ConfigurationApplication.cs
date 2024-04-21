779e30a71ee58382291699fc635b420c04c82e36
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-21 17:40:35 +0500

    add ConfigurationApplication

---
+++ src/Auth.Application/ConfigurationApplication.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Auth.Application.Security;
+using Auth.Application.Security.Interfaces;
+using Auth.Application.Services;
+using Auth.Application.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth.Application;
+
+public static class ConfigurationApplication
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IRefreshTokenService, RefreshTokenService>();
+        services.AddScoped<IPasswordManager, PasswordManager>();
+        services.AddScoped<ITokenManager, TokenManager>();
+        services.AddMediatR(x => x.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
