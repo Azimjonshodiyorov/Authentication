0a5c4affd3655ebd46abab1e1eba5cd7c72f4749
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-21 17:06:14 +0500

    RegisterCommand added

---
+++ src/Auth.Application/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+
+namespace Auth.Application.Commands.Register;
+
+public record RegisterCommand : IRequest<Guid>
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [MinLength(8)]
+    [MaxLength(20)]
+    public string Password { get; set; }
+}
---
+++ src/Auth.Application/Commands/Register/RegisterCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Auth.Application.Exceptions;
+using Auth.Application.Security.Interfaces;
+using Auth.Core.Entities;
+using Auth.Core.Enum;
+using Auth.Infrastructure.Repositories.Interfaces;
+using MediatR;
+
+namespace Auth.Application.Commands.Register;
+
+public class RegisterCommandHandler : IRequestHandler<RegisterCommand ,Guid>
+{
+    private readonly IUserRepository<User, Guid> _userRepository;
+    private readonly IPasswordManager _passwordManager;
+    private readonly IRoleRepository<Role, Guid> _roleRepository;
+
+    public RegisterCommandHandler(IUserRepository<User , Guid> userRepository , IPasswordManager passwordManager , IRoleRepository<Role , Guid> roleRepository)
+    {
+        _userRepository = userRepository;
+        _passwordManager = passwordManager;
+        _roleRepository = roleRepository;
+    }
+    public async Task<Guid> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        if (await _userRepository.GetUserByEmailAsync(request.Email) is not null)
+            throw new AlreadyExistException(nameof(User), request.Email);
+
+        this._passwordManager.CreatePasswordHash(request.Password, out var hash, out var salt);
+         var user = await this._userRepository.CreateAsync(
+             new User()
+             {
+                 Email = request.Email,
+                 PasswordHash = hash,
+                 PasswordSalt = salt,
+                 Role = await this._roleRepository.GetRoleByValueAsync(RoleName.User)
+                        ?? new Role { RoleName = RoleName.User },
+             });
+         return user.Id;
+    }
+}
