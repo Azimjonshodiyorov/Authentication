38f7482618f1c48fc67819d6462e7ed604d65572
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-21 13:06:37 +0500

    create token manager

--- src/Auth.Application/Security/TokenManager.cs
+++ src/Auth.Application/Security/TokenManager.cs
@@ -1,7 +1,11 @@
-﻿using System.Security.Cryptography;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using Auth.Application.Security.Interfaces;
 using Auth.Core.Entities;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Auth.Application.Security;
 
@@ -15,7 +19,28 @@ public class TokenManager : ITokenManager
     }
     public async ValueTask<string> GenerateAccessToken(User user)
     {
-        return null;
+        var claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Role, user.Role.RoleName.ToString())
+        };
+
+        var settings = _configuration.GetSection("Authentication:Key").Value!;
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var descriptore = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(int.Parse(_configuration.GetSection("Authentication:TokenLifeTimeMin")
+                .Value!)),
+            SigningCredentials = credentials,
+        };
+
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.CreateToken(descriptore);
+        return handler.WriteToken(token);
     }
 
     public RefreshToken GeneratRefreshToken(User user)
