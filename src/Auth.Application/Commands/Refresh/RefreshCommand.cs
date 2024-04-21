e3872b9d3100d83ea5c1c730f5e6423a6c8f25c2
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-21 16:55:40 +0500

    Added refresh command handler

---
+++ src/Auth.Application/Commands/Refresh/RefreshCommand.cs
@@ -0,0 +1,6 @@
+﻿using Auth.Application.Models;
+using MediatR;
+
+namespace Auth.Application.Commands.Refresh;
+
+public record RefreshCommand(string RefreshToken) : IRequest<AuthenticationResponse>;
---
+++ src/Auth.Application/Commands/Refresh/RefreshCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Auth.Application.Exceptions;
+using Auth.Application.Models;
+using Auth.Application.Security.Interfaces;
+using Auth.Application.Services.Interfaces;
+using Auth.Core.Entities;
+using Auth.Infrastructure.Repositories.Interfaces;
+using MediatR;
+
+namespace Auth.Application.Commands.Refresh;
+
+public class RefreshCommandHandler : IRequestHandler<RefreshCommand , AuthenticationResponse>
+{
+    private readonly ITokenManager _tokenManager;
+    private readonly IUserRepository<User, Guid> _userRepository;
+    private readonly IRefreshTokenService _refreshTokenService;
+
+    public RefreshCommandHandler(
+        ITokenManager tokenManager ,
+        IUserRepository<User , Guid> userRepository ,
+        IRefreshTokenService refreshTokenService)
+    {
+        _tokenManager = tokenManager;
+        _userRepository = userRepository;
+        _refreshTokenService = refreshTokenService;
+    }
+    public async Task<AuthenticationResponse> Handle(RefreshCommand request, CancellationToken cancellationToken)
+    {
+        var user = await this._userRepository.GetUserByRefreshTokenAsync(request.RefreshToken)
+                   ?? throw new UnauthorizedException("Refersh Token isn't valid");
+
+        await this._refreshTokenService.ValidateAndRemoveRefreshToken(user, request.RefreshToken);
+        var accessToken = await this._tokenManager.GenerateAccessToken(user);
+        var refreshToken =  this._tokenManager.GeneratRefreshToken(user);
+        user.RefreshTokens.Add(refreshToken);
+        await this._userRepository.UpdateAsync(user);
+
+        return new AuthenticationResponse(
+            new JwtToken(accessToken),
+            new CookieToken(refreshToken.Token, refreshToken.Expires));
+
+    }
+}
